8fda8dd2f4b22e6eba7f0c77e9a9d888aabec825
IanDCarroll <ian.d.carroll@gmail.com>
2016-06-09 16:14:06 -0700

    add js paow()

--- meth.js
+++ meth.js
@@ -32,8 +32,20 @@ function qoob(number) {
 	var cube = timz(sqar(number),number);
 	return cube;
 }
+
+function paow(number,exponent) {
+	var result = 1;
+	while (exponent > 0) {
+	    result = timz(number,result);
+	    exponent -= 1;
+	}
+	return result;
+}
 //console.log(modd(122,60));
 //console.log(timz(6,7));
 //console.log(cDiv(525600,1440));
 //console.log(sqar(5));
-console.log(qoob(5));
+//console.log(qoob(5));
+console.log(paow(5,0));
+console.log(paow(10,2));
+console.log(paow(2,6));
