7ccacbd5fa975791780e598dcc4dc88c355b297b
IanDCarroll <ian.d.carroll@gmail.com>
2016-06-18 07:19:05 -0700

    port timz() to java

--- meth.java
+++ meth.java
@@ -1,4 +1,4 @@
-class meth {
+class ud {
     public static int modd(int numerator, int modulator) {
 	while (numerator > modulator) {
 	    numerator -= modulator;
@@ -6,7 +6,19 @@ class meth {
 	return numerator;
     }
 
+    public static int timz(int factor1, int factor2){
+	int product = 0;
+	while (factor2 > 0) {
+	    product += factor1;
+	    factor2 -= 1;
+	}
+	return product;
+    }
+}
+
+public class meth {
     public static void main(String[] args) {
-	System.out.println(modd(122,60));
+	System.out.println(ud.modd(137,60));
+	System.out.println(ud.timz(6,7));
     }
 }
