da64ebc60e9b733caead49cf05fd9fd739ee5f08
IanDCarroll <ian.d.carroll@gmail.com>
2016-06-19 01:53:46 -0700

    add cDiv() to Java meth.ud

--- meth.java
+++ meth.java
@@ -6,7 +6,7 @@ class ud {
 	return numerator;
     }
 
-    public static int timz(int factor1, int factor2){
+    public static int timz(int factor1, int factor2) {
 	int product = 0;
 	while (factor2 > 0) {
 	    product += factor1;
@@ -14,11 +14,21 @@ class ud {
 	}
 	return product;
     }
+
+    public static int cDiv(int numerator, int denominator) {
+	int quotient = 0;
+	while (numerator > 0) {
+	    quotient += 1;
+	    numerator -= denominator;
+	}
+	return quotient;
+    }
 }
 
 public class meth {
     public static void main(String[] args) {
 	System.out.println(ud.modd(137,60));
 	System.out.println(ud.timz(6,7));
+	System.out.println(ud.cDiv(42,6));
     }
 }
