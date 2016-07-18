94f6be0419a8e47aa332dfae185e7c95d427a77e
IanDCarroll <ian.d.carroll@gmail.com>
2016-07-18 13:57:40 -0700

    refactor meth.c

--- meth.c
+++ meth.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+int modd(int numerator, int modulator) {
+    while (numerator >= modulator) {
+	numerator -= modulator;
+    }
+    return numerator;
+}
+
 int main() {
     int numerator;
     int modulator;
@@ -9,10 +16,6 @@ int main() {
     printf("Enter modulator: ");
     scanf("%d", &modulator);
 
-    while (numerator >= modulator) {
-	numerator -= modulator;
-    }
-
-    printf("Modulus is %d\n", numerator);
+    printf("Modulus is %d\n", modd(numerator, modulator));
     return 0;
 }
